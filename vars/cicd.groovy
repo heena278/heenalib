e00b065c38608df2e65abe89d742ea449f7ae8c0
heena278 <124327847+heena278@users.noreply.github.com>
2023-02-23 15:34:05 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,8 @@ def newdeploy(jobname,ip,context)
 {
 sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+
+def newselenium(jobname)
+{
+sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
